16dcd4feb6f9ecd472d49ad05f9af4a7972ba7a9
mocchi <boom.boom.planet@gmail.com>
2019-01-03 18:16:59 +0900

    initial commit

---
+++ lib/nonono_sender.rb
@@ -0,0 +1,6 @@
+require "nonono_sender/version"
+
+module NononoSender
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nonono_sender/version.rb
@@ -0,0 +1,3 @@
+module NononoSender
+  VERSION = "0.1.0"
+end
